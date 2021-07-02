4cc8236de4aec7a37c3719439e3dbfaf61a1aa67
StuffyEvil <stuffy19091@yahoo.ca>
2021-07-02 17:28:48 -0400

    Name Set-up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mock-three'`, () => {
+  it(`should have as title 'mock-3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mock-three');
+    expect(app.title).toEqual('mock-3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mock-three app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mock-3 app is running!');
   });
 });
