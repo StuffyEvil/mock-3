96f5c8ddf2363a97f2c1d15a4d8069b27986f235
StuffyEvil <stuffy19091@yahoo.ca>
2021-07-07 17:31:02 -0400

    Home Page Finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselModule],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(config: NgbCarouselConfig)
+  {
+    // Configurate the properties of the Carousel
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
   }
 
+  ngOnInit(): void {}
 }
