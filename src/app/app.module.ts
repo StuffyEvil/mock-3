bc40b6f2ef8ce19c31b5438bf7ae739b4ae814bb
StuffyEvil <stuffy19091@yahoo.ca>
2021-07-08 15:55:25 -0400

    Page-not-found Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
@@ -16,7 +17,8 @@ import { AboutUsComponent } from './about-us/about-us.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
