4fe735309e4dd1c978792e960a4966716683a050
StuffyEvil <stuffy19091@yahoo.ca>
2021-07-11 23:27:44 -0400

    Product List Initial

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material.module';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule(
   {
-  declarations: [],
+  declarations: [
+    ProductListComponent
+  ],
   imports: [
     SharedModule,
     MaterialModule,
