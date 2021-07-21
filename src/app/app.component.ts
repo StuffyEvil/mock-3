0ec5859732edfb06fbf41b3208ce464ca9fd7f4e
StuffyEvil <stuffy19091@yahoo.ca>
2021-07-21 11:55:10 -0400

    Mat Badge for Shopping Cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShoppingCart } from './data-and-extraction/shopping-cart/shopping-cart';
+import { ShoppingCartService } from './data-and-extraction/shopping-cart/shopping-cart.service';
 
 
 @Component(
@@ -7,7 +10,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
+  // Fields:
+
   title = 'mock-3';
+
+  // Error Message to store errors.
+  errorMessage = '';
+
+  // Shopping Cart Observable:
+  shoppingObs$: Observable<ShoppingCart>;
+
+  // cart:
+  cart: ShoppingCart;
+
+  // shoppingService Subscription:
+  shoppingSub$;
+
+
+  // Constructor:
+  constructor(private shoppingService: ShoppingCartService) {}
+
+
+  // On Initialization:
+  ngOnInit(): void
+  {
+    // Set up the Shopping Cart Observable.
+    this.shoppingObs$ = this.shoppingService.getShoppingCart();
+
+    // Setup Subscription:
+    this.shoppingSub$ = this.shoppingObs$.subscribe(
+    {
+      next: cart =>
+      {
+        this.cart = cart;
+      },
+      error: err => this.errorMessage = err
+    })
+  }
 }
