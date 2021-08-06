513cdcf75ea9bb59a90530d379b789153fe9300a
StuffyEvil <stuffy19091@yahoo.ca>
2021-08-06 09:49:16 -0400

    Rearranged the structure of the dialogue

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Inject, } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Product, ProductResolved } from 'src/app/data-and-extraction/product/product';
@@ -8,7 +7,10 @@ import { Review, Reviews } from 'src/app/data-and-extraction/review/review';
 import { ReviewsService } from 'src/app/data-and-extraction/review/reviews.service';
 import { ShoppingCartService } from 'src/app/data-and-extraction/shopping-cart/shopping-cart.service';
 import { GeneralValidators } from 'src/app/shared/general-validator';
-import { NewReviewDialogueComponent } from './new-review-dialogue/new-review-dialogue.component';
+
+
+
+// Product Detail
 
 @Component({
   selector: 'app-product-detail',
@@ -205,3 +207,93 @@ export class ProductDetailComponent implements OnInit, OnDestroy
   }
 
 }
+
+
+
+
+// New Review Dialogue
+
+@Component({
+  selector: 'app-new-review-dialogue',
+  templateUrl: './new-review-dialogue.component.html',
+  styleUrls: ['./new-review-dialogue.component.css']
+})
+export class NewReviewDialogueComponent
+{
+  // Fields:
+
+  // reviewContent:
+  _reviewContent: string;
+
+  // rating:
+  _rating: number;
+
+  // ratingFormControl:
+  // Form Control for ratings.
+  ratingFormControl: FormControl = new FormControl(
+  [
+    Validators.required,
+    GeneralValidators.notInt,
+    GeneralValidators.withinRange(0,5),
+  ]);
+
+
+  // Constructor:
+  constructor(
+    public newReviewDialogueRef: MatDialogRef<NewReviewDialogueComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: number) { }
+
+
+  // cancel:
+  // Cancels making a new Review.
+  cancel(): void
+  {
+    this.newReviewDialogueRef.close();
+  }
+
+
+  // makeReview:
+  // Takes the content of the dialogue and construct a Review, which is
+  // then returned.
+  makeReview(): void
+  {
+    // Create the new review
+    var newReview = {
+      'id': this.data,
+      'review': this._reviewContent,
+      'rating': this._rating,
+    };
+
+    // Close the dialogue with the new review as output.
+    this.newReviewDialogueRef.close(newReview);
+  }
+
+
+  // Getters & Setters:
+
+  // reviewContent:
+
+  get reviewContent(): string
+  {
+    return this._reviewContent;
+  }
+
+  set reviewContent(value: string)
+  {
+    this._reviewContent = value;
+  }
+
+  // rating:
+
+  get rating(): number
+  {
+    return this._rating;
+  }
+
+  set rating(value: number)
+  {
+    this._rating = value;
+  }
+
+}
+
