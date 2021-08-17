d0875ca6e2d9638e28fdceddbff0193c6e7b4363
StuffyEvil <stuffy19091@yahoo.ca>
2021-08-16 21:13:02 -0400

    Gotten Theme Toggle working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ShoppingCart } from './data-and-extraction/shopping-cart/shopping-cart';
 import { ShoppingCartService } from './data-and-extraction/shopping-cart/shopping-cart.service';
 
 
 @Component(
-  {
+{
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit
   // Error Message to store errors.
   errorMessage = '';
 
+
   // Shopping Cart Observable:
   shoppingObs$: Observable<ShoppingCart>;
 
@@ -29,8 +31,18 @@ export class AppComponent implements OnInit
   shoppingSub$;
 
 
+  // Set up theme changing elements, or at least try.
+  @HostBinding('class') componentCssComponent;
+
+  // Boolean to see which is the current theme.
+  // If it's false then it's Light, it it's true it's Dark.
+  themeToggle: boolean;
+
+
+
   // Constructor:
-  constructor(private shoppingService: ShoppingCartService) {}
+  constructor(private shoppingService: ShoppingCartService,
+              public overlayContainer: OverlayContainer) {}
 
 
   // On Initialization:
@@ -48,5 +60,35 @@ export class AppComponent implements OnInit
       },
       error: err => this.errorMessage = err
     })
+  }
+
+
+  // ToggleTheme:
+  // For changing the theme accordingly.
+  toggleTheme(): void
+  {
+    // If themeToggle is true, it's dark-theme so switch to light-theme.
+    if (this.themeToggle)
+    {
+      // Change Theme:
+      this.overlayContainer.getContainerElement().
+        classList.add('light-theme');
+      this.componentCssComponent = 'light-theme';
+
+      // Toggle themeToggle.
+      this.themeToggle = !(this.themeToggle);
+    }
+
+    // Else, themeToggle is false, so go from light-theme to dark-theme.
+    else
+    {
+      // Change Theme:
+      this.overlayContainer.getContainerElement().
+        classList.add('dark-theme');
+      this.componentCssComponent = 'dark-theme';
+
+      // Toggle themeToggle.
+      this.themeToggle = !(this.themeToggle);
+    }
   }
 }
